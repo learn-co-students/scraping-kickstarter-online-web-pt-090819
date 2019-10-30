4fe38f0331d8d6ea267a12a9229d654e23603089
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-10-30 00:47:49 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,18 @@
-# require libraries/modules here
+require 'pry'
+require 'nokogiri'
 
 def create_project_hash
-  # write your code here
+  
+  html = File.read('fixtures/kickstarter.html')
+ 
+  kickstarter = Nokogiri::HTML(html)
+  kickstarter.css("li.project.grid_4").each do |project|
+#    binding.pry
 end
+  
+ # binding.pry
+end
+
+create_project_hash
+
+# projects: kickstarter.css("li.project.grid_4")
